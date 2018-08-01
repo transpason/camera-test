a032bcbd9f0fe17eb0119e7eb82eaee8668da6e2
transpason <40496017+transpason@users.noreply.github.com>
2018-08-01 17:01:21 +0900

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,15 @@
+// スタートのボタン
+const $start = document.getElementById('s');
+// video要素
+const $video = document.getElementById('v');
+
+$start.addEventListener('click', () => {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(stream => $video.srcObject = stream)
+    .catch(err => alert(`${err.name} ${err.message}`));
+}, false);
+
+navigator.mediaDevices.enumerateDevices()
+  .then(devices => {
+    console.log(JSON.stringify(devices, undefined, 2));
+  });
