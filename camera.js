0195413b8c7d479ca528ab07e09f17ac74756476
transpason <40496017+transpason@users.noreply.github.com>
2018-08-02 11:37:06 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,7 +1,14 @@
-// video要素
+// htmlの要素
 const $canvas = document.getElementById('cvs'),
       $ctx = $canvas.getContext("2d"),
       $video = document.getElementById('vid');
+
+// posenet設定
+var imageScaleFactor = 0.5;
+var outputStride = 16;
+var flipHorizontal = false;
+var scale = 1.0;
+var color = 'pink';
 
 // 選択可能なデバイスをログ出力
 navigator.mediaDevices.enumerateDevices()
@@ -16,6 +23,17 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
 
 // canvasに描画
 requestAnimationFrame(draw);
+
+posenet.load()
+      .then(function(net){return net.estimateSinglePose($video, imageScaleFactor, flipHorizontal, outputStride)})
+      .then(function(pose){
+          var keypoints = pose.keypoints;
+          for (let i = 0; i < keypoints.length; i++) {
+            const keypoint = keypoints[i];
+            const {y, x} = keypoint.position;
+            drawPoint($ctx, y * scale, x * scale, 3, color);
+          }
+      });
 
 function draw() {
   $canvas.width  = window.innerWidth;
