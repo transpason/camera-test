f5f14b8097947bd87d1d40f8a2ccbee642220ac9
transpason <40496017+transpason@users.noreply.github.com>
2018-08-02 12:14:48 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,44 +1,173 @@
-// htmlの要素
-const $canvas = document.getElementById('cvs'),
-      $ctx = $canvas.getContext("2d"),
-      $video = document.getElementById('vid');
-
 // posenet設定
-var imageScaleFactor = 0.5;
-var outputStride = 16;
-var flipHorizontal = false;
-var scale = 1.0;
-var color = 'pink';
+const videoWidth = 600;
+const videoHeight = 500;
 
-// 選択可能なデバイスをログ出力
-navigator.mediaDevices.enumerateDevices()
-  .then(devices => {
-    console.log(JSON.stringify(devices, undefined, 2));
-  });
-
-// カメラを起動
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  .then(stream => $video.srcObject = stream)
-  .catch(err => alert(`${err.name} ${err.message}`));
-
-// canvasに描画
-requestAnimationFrame(draw);
-
-posenet.load()
-      .then(function(net){return net.estimateSinglePose($video, imageScaleFactor, flipHorizontal, outputStride)})
-      .then(function(pose){
-          var keypoints = pose.keypoints;
-          for (let i = 0; i < keypoints.length; i++) {
-            const keypoint = keypoints[i];
-            const {y, x} = keypoint.position;
-            drawPoint($ctx, y * scale, x * scale, 3, color);
-          }
-      });
-
-function draw() {
-  $canvas.width  = window.innerWidth;
-  $canvas.height = window.innerHeight;
-  $ctx.drawImage($video, 0, 0);
-
-  requestAnimationFrame(draw);
+function isAndroid() {
+  return /Android/i.test(navigator.userAgent);
 }
+
+function isiOS() {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+function isMobile() {
+  return isAndroid() || isiOS();
+}
+
+// Loads a the camera to be used in the demo
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+        'Browser API navigator.mediaDevices.getUserMedia not available');
+  }
+
+  const video = document.getElementById('vid');
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  const mobile = isMobile();
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      width: mobile ? undefined : videoWidth,
+      height: mobile ? undefined : videoHeight,
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function loadVideo() {
+  const video = await setupCamera();
+  video.play();
+
+  return video;
+}
+
+/**
+ * Feeds an image to posenet to estimate poses - this is where the magic
+ * happens. This function loops with a requestAnimationFrame method.
+ */
+function detectPoseInRealTime(video, net) {
+  const canvas = document.getElementById('cvs');
+  const ctx = canvas.getContext('2d');
+  // since images are being fed from a webcam
+  const flipHorizontal = true;
+
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  async function poseDetectionFrame() {
+    // Scale an image down to a certain factor. Too large of an image will slow
+    // down the GPU
+    const imageScaleFactor = 0.5;
+    const outputStride = 16;
+    const maxPoseDetections = 2;
+    const minPoseConfidence = 0.15;
+    const minPartConfidence = 0.1;
+
+    let poses = [];
+    poses = await net.estimateMultiplePoses(
+        video, imageScaleFactor, flipHorizontal, outputStride, maxPoseDetections
+    );
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.translate(-videoWidth, 0);
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+    ctx.restore();
+
+    // For each pose (i.e. person) detected in an image, loop through the poses
+    // and draw the resulting skeleton and keypoints if over certain confidence
+    // scores
+    poses.forEach(({score, keypoints}) => {
+      if (score >= minPoseConfidence) {
+        drawKeypoints(keypoints, minPartConfidence, ctx);
+        drawSkeleton(keypoints, minPartConfidence, ctx);
+      }
+    });
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+/**
+ * Kicks off the demo by loading the posenet model, finding and loading
+ * available camera devices, and setting off the detectPoseInRealTime function.
+ */
+export async function bindPage() {
+  // Load the PoseNet model weights with architecture 0.75
+  const net = await posenet.load(0.5);
+
+  let video;
+
+  try {
+    video = await loadVideo();
+  } catch (e) {
+    let info = document.getElementById('info');
+    info.textContent = 'this browser does not support video capture,' +
+        'or this device does not have a camera';
+    info.style.display = 'block';
+    throw e;
+  }
+
+  detectPoseInRealTime(video, net);
+}
+
+function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+
+    const {y, x} = keypoint.position;
+    drawPoint(ctx, y * scale, x * scale, 3, color);
+  }
+}
+
+function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints, minConfidence);
+
+  adjacentKeyPoints.forEach((keypoints) => {
+    drawSegment(toTuple(keypoints[0].position),
+      toTuple(keypoints[1].position), color, scale, ctx);
+  });
+}
+
+function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+function toTuple({y, x}) {
+  return [y, x];
+}
+
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+// kick off the demo
+bindPage();
