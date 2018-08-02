55f4748179b1795644695341a12fefbb616799d0
transpason <40496017+transpason@users.noreply.github.com>
2018-08-02 10:59:27 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,15 +1,26 @@
-// スタートのボタン
-const $start = document.getElementById('s');
 // video要素
-const $video = document.getElementById('v');
+const $canvas = document.getElementById('cvs'),
+      $ctx = $canvas.getContext("2d"),
+      $video = document.getElementById('vid');
 
-$start.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(stream => $video.srcObject = stream)
-    .catch(err => alert(`${err.name} ${err.message}`));
-}, false);
-
+// 選択可能なデバイスをログ出力
 navigator.mediaDevices.enumerateDevices()
   .then(devices => {
     console.log(JSON.stringify(devices, undefined, 2));
   });
+
+// カメラを起動
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  .then(stream => $video.srcObject = stream)
+  .catch(err => alert(`${err.name} ${err.message}`));
+
+// canvasに描画
+requestAnimationFrame(draw);
+
+function draw() {
+  $canvas.width  = window.innerWidth;
+  $canvas.height = window.innerHeight;
+  $ctx.drawImage(video, 0, 0);
+
+  requestAnimationFrame(draw);
+}
